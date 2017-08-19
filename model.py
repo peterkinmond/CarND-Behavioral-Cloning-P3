55ac984a90c50fa1679d5c72b40a75b8d3ab5a5c
Peter Kinmond <peterkinmond@gmail.com>
2017-08-19 15:33:01 -0700

    Test out NVIDIA architecture

--- model.py
+++ model.py
@@ -47,19 +47,26 @@ from keras.layers.pooling import MaxPooling2D
 model = Sequential()
 
 # Crop the irrelevant part of the images (top tree line, bottom car pixels)
-model.add(Cropping2D(cropping=((80, 20), (0, 0)), input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70, 25), (0, 0)), input_shape=(160,320,3)))
 # pixel_normalized = pixel / 255.0 # shifts values into 0 to 1 range
 # pixel_mean_centered = pixel_normalized - 0.5 # shifts values into -0.5 to 0.5 range
 model.add(Lambda(lambda x: x / 255.0 - 0.5))
 
-model.add(Conv2D(6, (5, 5), activation="relu"))
+model.add(Conv2D(24, (5, 5), activation="relu"))
 model.add(MaxPooling2D())
-model.add(Conv2D(6, (5, 5), activation="relu"))
+model.add(Conv2D(36, (5, 5), activation="relu"))
 model.add(MaxPooling2D())
+model.add(Conv2D(48, (5, 5), activation="relu"))
+model.add(MaxPooling2D())
+model.add(Conv2D(64, (3, 3), activation="relu"))
+# model.add(Conv2D(64, (3, 3), activation="relu"))
+
 model.add(Flatten())
-model.add(Dense(120))
+model.add(Dense(100))
 model.add(Dropout(0.5))
-model.add(Dense(84))
+model.add(Dense(50))
+model.add(Dropout(0.5))
+model.add(Dense(10))
 model.add(Dropout(0.5))
 model.add(Dense(1))
 
